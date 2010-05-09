151595f5238c38df7e9ef334aa6148fa9bdf32e6
root <root@andLinux.(none)>
2010-05-09 00:09:15 -0400

    child widget

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@
  int main(int argc, char *argv[])
  {
      QApplication app(argc, argv);
-     QWidget window;
-     window.resize(320, 240);
-     window.show();
-     window.setWindowTitle(
-         QApplication::translate("toplevel", "Top-level widget"));
-     return app.exec();
+          QWidget window;
+          window.resize(320, 240);
+          window.setWindowTitle(QApplication::translate("childwidget", "Child widget"));
+          window.show();
+
+          QPushButton *button = new QPushButton(
+              QApplication::translate("childwidget", "Press me"), &window);
+          button->move(100, 100);
+          button->show();
+          return app.exec();
  }
